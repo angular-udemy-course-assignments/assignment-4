ec85f4d3aa8d58094e2df819501f6b7244d5b689
nkolchakov <nikola.kolchakov@gmail.com>
2019-02-07 15:36:25 +0200

    my solution

---
+++ cmp-databinding-assignment-start/src/app/game-control/game-control.component.ts
@@ -0,0 +1,34 @@
+import {Component, EventEmitter, Input, OnDestroy, OnInit, Output} from '@angular/core';
+import {TimeInterval} from 'rxjs';
+
+@Component({
+  selector: 'app-game-control',
+  templateUrl: './game-control.component.html',
+  styleUrls: ['./game-control.component.css']
+})
+export class GameControlComponent implements OnInit, OnDestroy {
+  counter = 0;
+  interval: any;
+  seconds = 1 * 1000;
+
+  @Input() gameComponents;
+  @Output() gameStartEmitter = new EventEmitter<number>();
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  onGameStart() {
+    this.interval = setInterval(() => {
+      this.gameStartEmitter.emit(this.counter++);
+      }, this.seconds);
+  }
+
+  onGameStop() {
+    clearInterval(this.interval);
+  }
+
+  ngOnDestroy() {
+    this.onGameStop();
+  }
+}
